ea3a48531454b00539e79eddf2cd55b1d51e7118
Kellen James <kellenjames@Kellens-MBP.attlocal.net>
2017-08-13 17:11:17 -0700

    mySQL schema updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name, devoured, ts)
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon blue cheese burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("chipotle turkey burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("taco burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("chicken caesar burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("canyon creek burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("teriyaki burger", true);
 
-VALUES ("bacon", true, '2008-01-01 00:00:01');
