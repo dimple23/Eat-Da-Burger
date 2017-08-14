ff40cccb5c8b4511e0835bfa89fd6bae79221996
Kellen James <kellenjames@Kellens-MBP.attlocal.net>
2017-08-13 17:20:51 -0700

    burger.js virtual orm model has been updated

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,23 @@
-/* Here is where you setup a model for how to interface with the database */
+/* Here is where you setup a virtual model for how to interface with the database */
 
 var orm = require('../config/orm.js');
 
+//Create the burger object
 var burger = {
-	all: function (cb) {
-		orm.all('burgers', function (res){
+	selectAll: function (cb) {
+		orm.selectAll('burgers', function (res){
 			cb(res);
 		});
 	},
+	
 	// cols and vals are arrays
-	create: function (cols, vals, cb) {
+	insertOne: function (cols, vals, cb) {
 		orm.create('burgers', cols, vals, function(res) {
 			cb(res);
 		});
 	},
-	update: function (objColVals, condition, cb) {
+	updateOne: function (objColVals, condition, cb) {
 		orm.update('burgers', objColVals. condition, function(res){
-			cb(res);
-		});
-	},
-	delete: function (condition, cb) {
-		orm.delete('burgers', condition, function(res){
 			cb(res);
 		});
 	}
