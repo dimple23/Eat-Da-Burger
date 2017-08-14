9d72b029b5e8b055decd0f20532f02ceb015d09b
Kellen James <kellenjames@Kellens-MBP.attlocal.net>
2017-08-13 23:54:13 -0700

    JawsDB remote config added

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 // DEPENDENCIES // 
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database:'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}	else {
+	var connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database:'burgers_db'
+	});
+}
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
 });
+
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
