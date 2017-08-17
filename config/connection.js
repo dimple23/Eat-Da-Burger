073745b0295e6491254ba4794721b665372dcdf7
Kellen <Kellenkjames@gmail.com>
2017-08-16 21:27:06 -0700

    connection.js modified

--- config/connection.js
+++ config/connection.js
@@ -24,5 +24,7 @@ connection.connect(function(err) {
 	console.log("connected as id " + connection.threadId);
 });
 
+
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
